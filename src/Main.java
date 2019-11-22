588d77aaa1ea8f1ace129e2dfc1e63a399e3288d
Amós Lima <academico.amos@gmail.com>
2019-11-22 16:55:28 -0300

    added controllers; added resources;

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,9 +8,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Parent login = FXMLLoader.load(getClass().getResource("../resources/loginUflib.fxml"));
+
+        Scene loginform = new Scene(login);
+
+        primaryStage.setTitle("BIBLIOTECA - UFPI");
+        primaryStage.setScene(loginform);
         primaryStage.show();
     }
 
