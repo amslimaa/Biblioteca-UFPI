4ef6ddd696d2a490a32e1c605f173c4964847794
Amós Lima <academico.amos@gmail.com>
2019-11-23 14:40:01 -0300

    finish sign/sigup

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,15 +1,26 @@
 package controllers;
 
 import animatefx.animation.ZoomIn;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 
 
 public class LoginController {
+
+
+    ObservableList<String> LocatarioTipo = FXCollections.observableArrayList("Aluno", "Professor", "TecAdministrativo");
+
+
+    @FXML
+    private ChoiceBox CadChoiceBox = null;
 
     @FXML
     private Pane pnlLogin = null;
@@ -24,6 +35,15 @@ public class LoginController {
     private Circle btnClose = null;
     @FXML
     private Button btnCad = null;
+
+
+    @FXML
+    public void initialize(){
+        CadChoiceBox.setValue("Vinculo");
+        CadChoiceBox.setItems(LocatarioTipo);
+
+    }
+
     public void handleButtonAction(ActionEvent event) {
         if(event.getSource().equals(btnCad)){
             new ZoomIn(pnlCad).play();
